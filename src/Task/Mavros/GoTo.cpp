8b99e05ac003a1f0fb3b3b510a67f9b8e221a8be
afonsofonbraga <afonso.braga@engenharia.ufjf.br>
2020-08-25 10:37:22 -0300

    add mavros support

---
+++ src/Task/Mavros/GoTo.cpp
@@ -0,0 +1,9 @@
+//
+//  GoTo.cpp
+//  MRSFramework
+//
+//  Created by Afonso Braga on 25/08/20.
+//  Copyright © 2020 Afonso Braga. All rights reserved.
+//
+
+#include "GoTo.hpp"
---
+++ src/Task/Mavros/GoTo.hpp
@@ -0,0 +1,14 @@
+//
+//  GoTo.hpp
+//  MRSFramework
+//
+//  Created by Afonso Braga on 25/08/20.
+//  Copyright © 2020 Afonso Braga. All rights reserved.
+//
+
+#ifndef GoTo_hpp
+#define GoTo_hpp
+
+#include <stdio.h>
+
+#endif /* GoTo_hpp */
