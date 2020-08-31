81700a7aa0bd71d8db36e295b91bacab3d1d67e3
afonsofonbraga <afonso.braga@engenharia.ufjf.br>
2020-08-31 16:21:42 -0300

    Add DecomposableTasks methods

---
+++ src/Task/Rosbot/DecomposableTasks.hpp
@@ -0,0 +1,26 @@
+//
+//  DecomposableTasks.hpp
+//  MRSMac
+//
+//  Created by Afonso Braga on 31/08/20.
+//  Copyright © 2020 Afonso Braga. All rights reserved.
+//
+
+#ifndef DecomposableTasks_hpp
+#define DecomposableTasks_hpp
+
+#include <stdio.h>
+#include "BlackBoard.hpp"
+#include "MissionExecution.hpp"
+
+
+#include "GoTo.hpp"
+#include "ChargeBattery.hpp"
+#include "TurnOn.hpp"
+#include "TakePicture.hpp"
+
+
+
+void decomposableTaskList(BlackBoard* monitor);
+void addAtomicTask2(BlackBoard* monitor, MissionExecution& vMissionDecomposable);
+#endif /* DecomposableTasks_hpp */
---
+++ src/Task/Turtlebot/DecomposableTasks.cpp
@@ -0,0 +1,57 @@
+//
+//  DecomposableTasks.cpp
+//  MRSMac
+//
+//  Created by Afonso Braga on 31/08/20.
+//  Copyright © 2020 Afonso Braga. All rights reserved.
+//
+
+#include "DecomposableTasks.hpp"
+
+void decomposableTaskList(BlackBoard* monitor)
+{
+
+    std::vector<enum_AtomicTask> teste;
+    teste.push_back(enum_AtomicTask::goTo);
+    enum_DecomposableTask lala = enum_DecomposableTask::checkPosition;
+    monitor->addDecomposableTaskList(lala, teste);
+    
+    teste.clear();
+    teste.push_back(enum_AtomicTask::chargeBattery);
+    lala = enum_DecomposableTask::lowBattery;
+    
+    monitor->addDecomposableTaskList(lala, teste);
+    
+}
+
+void addAtomicTask2(BlackBoard* monitor, MissionExecution& vMissionDecomposable)
+{
+    vMissionDecomposable.atomicTaskList.clear();
+        std::shared_ptr<AtomicTask> vAtomicTaskitem = nullptr;
+        s_pose currentPosition;
+        monitor->getPosition(currentPosition);
+        
+        for (auto n : vMissionDecomposable.vAtomicTaskVector){
+            switch(n){
+                case enum_AtomicTask::null :
+                    break;
+                case enum_AtomicTask::goTo :
+                    vAtomicTaskitem = std::make_shared<GoTo>(monitor, currentPosition,vMissionDecomposable.goal);
+                    currentPosition = vMissionDecomposable.goal;
+                    break;
+                case enum_AtomicTask::turnOn :
+                    vAtomicTaskitem = std::make_shared<TurnOn>(monitor, currentPosition,currentPosition);
+                    break;
+                case enum_AtomicTask::chargeBattery :
+                    vAtomicTaskitem = std::make_shared<ChargeBattery>(monitor, currentPosition,currentPosition);
+                    break;        
+            }
+            if (vAtomicTaskitem != nullptr)
+            {
+                vMissionDecomposable.atomicTaskList.push_back(std::move(vAtomicTaskitem));
+                //delete vAtomicTaskitem;
+            } else {
+                std::cout << "Not found\n";
+            }
+        }
+}
