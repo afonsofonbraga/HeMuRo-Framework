9ac13753baf548dd23b28feb569fdcae44859c16
Afonso Braga <afonso.braga@engenharia.ufjf.br>
2021-04-19 16:04:02 -0300

    Adding graphics for charging process

--- results/graphics.py
+++ results/graphics.py
@@ -12,8 +12,8 @@ def autolabel(rects, axss):
                     textcoords="offset points",
                     ha='center', va='bottom')
                     
-hand = open('/home/robot/github/HeMuRo-Framework/logs/Logger131/Missions.txt')
-#hand = open('/Users/afonsofonbraga/github/HeMuRo-Framework/logs/Logger49/Missions.txt')
+#hand = open('/home/robot/github/HeMuRo-Framework/logs/Logger131/Missions.txt')
+hand = open('/Users/afonsofonbraga/github/HeMuRo-Framework/logs/Logger120/Missions.txt')
 mission_owner = dict()
 mission_executioner = dict()
 mission_redirected = dict()
@@ -21,6 +21,8 @@ mission_lowbattery = dict()
 mission_failure = dict()
 mission_timeout = dict()
 mission_null = dict()
+charging_request = dict()
+charging_station_count = dict()
 
 for line in hand:
     line = line.rstrip()
@@ -89,6 +91,31 @@ for line in hand:
             
             mission_failure[executioner] = mission_failure.get(executioner, 0) + 1
             
+# Creating a dicionary with all agents who Requested to Charge [agentsName] = Number of Charging requests
+    missions = re.findall('.*ChargingRequested$', line)
+    for mission in missions:
+        words = line.split(', ')
+        if len(words) == 7:
+            owner = words[1]
+            station = words[2]
+            deadline = int(words[3])
+            estimated = int(words[4])
+            realtime = int(words[5])
+            
+            charging_request[owner] = charging_request.get(owner, 0) + 1
+
+# Creating a dicionary with all agents who redirected at least one misison due to failure [agentsName] = Number of redirected missions
+    missions = re.findall('.*ChargingCompleted$', line)
+    for mission in missions:
+        words = line.split(', ')
+        if len(words) == 7:
+            station = words[2]
+            deadline = int(words[3])
+            estimated = int(words[4])
+            realtime = int(words[5])
+            
+            charging_station_count[station] = charging_station_count.get(station, 0) + 1
+            
 print("Mission Offered:")
 print(mission_owner)
 print("Mission Completed")
@@ -101,10 +128,17 @@ print("Mission Redirected - Timeout")
 print(mission_timeout)
 print("Mission Redirected - Failure")
 print(mission_failure)
+print("Charging Requests")
+print(charging_request)
+print("Charging Requests Completed for each station")
+print(charging_station_count)
 
-label_onwer = list(mission_owner.keys())
-size_onwer = list(mission_owner.values())
-explode_owner = [0] * len(size_onwer)
+# Adding missing agents in each dictinary in order to plot correctly
+
+for agent in list(mission_owner.keys()):
+    charging_request[agent] = charging_request.get(agent, 0 )
+for agent in list(charging_request.keys()):
+    mission_owner[agent] = mission_owner.get(agent, 0 )
 
 for agent in list(mission_redirected.keys()):
     mission_executioner[agent] = mission_executioner.get(agent, 0 )
@@ -118,6 +152,17 @@ for agent in list(mission_executioner.keys()):
 for agent in list(mission_executioner.keys()):
         mission_failure[agent] = mission_failure.get(agent, 0 )
 
+
+# criando o primeiro grafico
+label_onwer = list(mission_owner.keys())
+ordered_owner = list(mission_owner.values())
+
+#ordered_owner = list()
+ordered_charging_request = list()
+#criar na uma lista ordenada, com os valores de charging_station_count mas na ordem de mission_owner.keys()
+for agent in label_onwer:
+    ordered_charging_request.append(charging_request.get(agent))
+
 label_executioner = list(mission_executioner.keys())
 size_executioner = list(mission_executioner.values())
 explode_executioner = [0] * len(size_executioner)
@@ -129,23 +174,31 @@ ordered_failure = list()
 ordered_timeout = list()
 
 for agent in list(mission_redirected.keys()):
-	ordered_redirected.insert(-1,mission_redirected.get(agent))
+	ordered_redirected.append(mission_redirected.get(agent))
 for agent in list(mission_redirected.keys()):
-    ordered_lowbattery.insert(-1,mission_lowbattery.get(agent))
+    ordered_lowbattery.append(mission_lowbattery.get(agent))
 for agent in list(mission_redirected.keys()):
-    ordered_failure.insert(-1,mission_failure.get(agent))
+    ordered_failure.append(mission_failure.get(agent))
 for agent in list(mission_redirected.keys()):
-    ordered_timeout.insert(-1,mission_timeout.get(agent))
+    ordered_timeout.append(mission_timeout.get(agent))
 
 
 explode_redirected = [0] * len(ordered_redirected)
 
 fig, axs = plt.subplots(2, 2, figsize=(11, 6))
 
-rects0 = axs[0,0].bar(label_onwer, size_onwer)
-axs[0,0].axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+x = np.arange(len(label_onwer))  # the label locations
+width = 0.05  # the width of the bars
+
+rects0 = axs[0,0].bar(x - width/2, ordered_owner, width, label='Mission')
+rects01 = axs[0,0].bar(x + width/2, ordered_charging_request, width, label='Charging Requests')
+
 axs[0,0].set(title= "Mission Owners")
 axs[0,0].set_ylabel('Missions')
+axs[0,0].set_xticks(x)
+axs[0,0].set_xticklabels(label_onwer)
+axs[0,0].legend()
+
 
 axs[0,1].pie(size_executioner, explode=explode_executioner, labels=label_executioner, autopct='%1.1f%%', shadow=True, startangle=90)
 axs[0,1].axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
@@ -179,6 +232,7 @@ axs[1,1].legend()
 
 
 autolabel(rects0, axs[0,0])
+autolabel(rects01, axs[0,0])
 autolabel(rects1, axs[1,0])
 autolabel(rects2, axs[1,0])
 #autolabel(rects3, axs[1,1])
